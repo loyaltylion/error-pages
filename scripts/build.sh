8d987e20d5ee96f8f5575b4d4fbd822c8a068d0f
Dave Clark <me@clarkdave.net>
2017-07-05 21:00:42 +0100

    add 404 page

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,7 @@ for code in 500 501 502 503 504; do
   erb type=error code="$code" template.html.erb > "dist/$code.html"
 done
 
+erb type=not_found code=404 template.html.erb > dist/404.html
 erb type=maintenance template.html.erb > dist/maintenance.html
 
 for f in dist/*; do
